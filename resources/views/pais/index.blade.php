493dc82e7eca58f45b5009e9292f3d4f64ff5db7
Janeer barrera <barrera.janeer@gmail.com>
2025-03-27 16:24:05 -0500

    Creacion Rutas para Pais

--- resources/views/pais/index.blade.php
+++ resources/views/pais/index.blade.php
@@ -8,44 +8,11 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Listado de Paises</title>
+    <title>Hello, world!</title>
   </head>
   <body>
-  <div class="container">
-    <h1>Listado de Paises</h1>
-    <a href="{{route('paises.create') }}" class= "btn btn-success">Add</a>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Code</th>
-      <th scope="col">Country</th>
-      <th scope="col">Capital City</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-   @foreach ($paises as $pais)
-    <tr>
-      <th scope="row">{{ $pais->pais_codi}}</th>
-      <td> {{$pais->pais_nomb}}</td>
-      <td>{{ $pais->pais_capi}}</td>
-      <td><span>Actions</span></td>
-      <td>
-      <a href="{{ route('paises.edit', ['pais' => $pais->pais_codi]) }}"
-      class="btn btn-info">Edit</a>
+    <h1>Hello, world!</h1>
 
-     <form action="{{ route('paises.destroy', ['pais' => $pais->pais_codi]) }}"
-            method="POST" style="display: inline-block">
-          @method('delete')
-          @csrf
-          <input class="btn btn-danger" type="submit" value="Delete">
-      </form>
-      </td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-  </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
